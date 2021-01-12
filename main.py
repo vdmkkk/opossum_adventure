86ea3ebad470f34c46b94b6a708a370072f8864f
Vadim <soloviov.vad@gmail.com>
2021-01-13 01:57:18 +0300

    physics ✓ reddish ✓ test_level ✓ have to make coins system, game over screen and menu, that's gonna be it for the beta
    Signed-off-by: Vadim <soloviov.vad@gmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ pg.init()
 
 WIDTH = 1000
 HEIGHT = 1000
-TILE_SIZE = 200
+TILE_SIZE = 50
+game_over = False
+
+undg_bg = pg.image.load('source/undg_bg.png')
+out_bg = pg.image.load('source/out_bg.png')
 
 clock = pg.time.Clock()
 
@@ -12,11 +16,26 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('opossum adventure')
 
 
-map = [[2, 2, 2, 2, 2],
-       [1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1],
-       [1, 1, 1, 1, 1]]
+map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 
 class World():
@@ -32,11 +51,23 @@ class World():
         for row in map:
             col_count = 0
             for tile in row:
-                img = pg.transform.scale(images[tile - 1], (TILE_SIZE, TILE_SIZE))
-                img_rect = img.get_rect()
-                img_rect.x = col_count * TILE_SIZE
-                img_rect.y = row_count * TILE_SIZE
-                self.tile_list.append((img, img_rect))
+                if tile == 1:
+                    img = pg.transform.scale(dirt_img, (TILE_SIZE, TILE_SIZE))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * TILE_SIZE
+                    img_rect.y = row_count * TILE_SIZE
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pg.transform.scale(grass_img, (TILE_SIZE, TILE_SIZE))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * TILE_SIZE
+                    img_rect.y = row_count * TILE_SIZE
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    redish = Redish(col_count * TILE_SIZE, row_count * TILE_SIZE + (TILE_SIZE // 2))
+                    redishes.add(redish)
                 col_count += 1
             row_count += 1
 
@@ -47,53 +78,114 @@ class World():
 
 class Player():
     def __init__(self, x, y):
-        img = pg.image.load('source/opossum.png')
-        self.image = pg.transform.scale(img, (150, 150))
-        self.image.convert()
-        colorkey = self.image.get_at((1, 1))
-        self.image.set_colorkey(colorkey)
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 3):
+            img_right = pg.image.load(f'source/opossum{num}.png')
+            img_right = pg.transform.scale(img_right, (100, 60))
+            img_right.convert()
+            img_left = pg.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.dead_image = pg.transform.flip(self.images_right[0], False, True)
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.on_ground = True
+        self.dir = 0
 
     def update(self):
 
         dx = 0
         dy = 0
+        walk_cooldown = 20
 
-        key = pg.key.get_pressed()
+        global game_over
+        if not game_over:
+            key = pg.key.get_pressed()
 
-        if key[pg.K_LEFT]:
-            dx -= 6
-        if key[pg.K_RIGHT]:
-            dx += 6
-        if key[pg.K_UP] and self.on_ground:
-            self.vel_y = -25
-            self.on_ground = False
-        if not key[pg.K_UP]:
-            self.on_ground = True
+            # keys
+            if key[pg.K_LEFT]:
+                dx -= 4
+                self.counter += 1
+                self.dir = -1
+                self.image = self.images_left[self.index]
+            if key[pg.K_RIGHT]:
+                dx += 4
+                self.dir = 1
+                self.counter += 1
+                self.image = self.images_right[self.index]
+            if key[pg.K_UP] and self.on_ground:
+                self.vel_y = -20
+                self.on_ground = False
+            if not key[pg.K_UP]:
+                self.on_ground = True
+            if not key[pg.K_LEFT] and not key[pg.K_RIGHT]:
+                self.counter = 0
+                self.index = 0
 
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        dy += self.vel_y
+            # gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
 
-        self.rect.x += dx
-        self.rect.y += dy
+            # collision
+            for tile in world.tile_list:
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
 
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-            dy = 0
+            # collision with redish
+            if pg.sprite.spritecollide(self, redishes, False):
+                game_over = True
+
+            # animation
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.dir == 1:
+                    self.image = self.images_right[self.index]
+                if self.dir == -1:
+                    self.image = self.images_left[self.index]
+
+            # update
+            self.rect.x += dx
+            self.rect.y += dy
+        elif game_over:
+            self.image = self.dead_image
 
         screen.blit(self.image, self.rect)
 
 
+class Redish(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        img = pg.image.load('source/redish.png')
+        self.image = pg.transform.scale(img, (TILE_SIZE, TILE_SIZE // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 
+redishes = pg.sprite.Group()
 world = World(map)
-player = Player(100, HEIGHT - 200)
+player = Player(100, HEIGHT - 300)
+
 
 running = True
 while running:
@@ -101,7 +193,9 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    screen.blit(undg_bg, (0, 0))
     world.update()
+    redishes.draw(screen)
     player.update()
     pg.display.update()
 pg.quit()
